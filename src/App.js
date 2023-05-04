23af17b21bbdbf80e29d70c8ec0d71d80bcbb9d0
sawfiz <dashu.up@gmail.com>
2023-05-04 11:20:08 +0800

    Add form onChange and onSubmit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React, { Component } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-export default function TaskApp () {
+export default function TaskApp() {
+  const [task, setTask] = useState({ title: '' });
+  const [tasks, setTasks] = useState([]);
+  useEffect(() => {console.log(tasks)}, [tasks])
 
+  function handleChange(e) {
+    setTask({title: e.target.value})
+    console.log(e.target.value);
+  }
+  
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTasks([...tasks, task] )
+  }
 
   return (
     <div>
       <h1>Task App</h1>
-      <form>
-        <input type='text' placeholder='Enter a task'></input>
-        <button>Submit</button>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Enter a task" value={task.title} onChange={handleChange}></input>
+        <button type='submit'>Submit</button>
       </form>
     </div>
-  )
-
+  );
 }
