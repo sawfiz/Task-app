6fed4aa5c8619d57f85b9860b9bbbaeb8269136c
sawfiz <dashu.up@gmail.com>
2023-05-04 09:44:34 +0800

    Restart using functional components and useState

--- src/App.js
+++ src/App.js
@@ -1,51 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 
-export class App extends Component {
-  constructor() {
-    super();
+export default function TaskApp () {
 
-    this.state = {
-      task: { title: '' },
-      tasks: [],
-    };
-  }
 
-  handleChange(e) {
-    e.preventDefault();
-    console.log(e.target.value);
-    this.setState({ task: { title: e.target.value } });
-  }
+  return (
+    <div>
+      <h1>Task App</h1>
+      <form>
+        <input type='text' placeholder='Enter a task'></input>
+        <button>Submit</button>
+      </form>
+    </div>
+  )
 
-  handleSubmit(e) {
-    e.preventDefault();
-    console.log(this.state);
-
-    this.setState({
-      task: {title: ''},
-      // Construct a new array with spread operator `...` on this.state.tasks
-      // then add this.state.task to the array
-      tasks: [...this.state.tasks, this.state.task],
-    }, () =>  console.log(this.state));
-  }
-
-  render() {
-    const { task, tasks } = this.state;
-    return (
-      <div>
-        <h1>Task App</h1>
-        <form className="task-form" onSubmit={this.handleSubmit.bind(this)}>
-          <input
-            type="text"
-            placeholder="Enter a task here..."
-            value={task.title}
-            onChange={this.handleChange.bind(this)}
-          ></input>
-          <button type="submit">Add to List</button>
-        </form>
-      </div>
-    );
-  }
 }
-
-export default App;
